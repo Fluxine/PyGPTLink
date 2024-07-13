c487a3ec2990117d65bba687f349ff19a4167890
Fluxine <174742605+Fluxine@users.noreply.github.com>
2024-07-13 14:47:07 +0200

    Update the simple example code.

--- examples/simple_usage.py
+++ examples/simple_usage.py
@@ -1,4 +1,5 @@
 import asyncio
+import pprint
 import sys
 import os
 
@@ -22,23 +23,56 @@ def callback(sentence: str, response_done: bool):
         response_done: true only on the last sentence of the response.
 
     """
-    print(f"[{sentence}] response_done: {response_done}")
+    print(f"Assistant: {sentence} (response_done: {response_done})")
 
 
 async def main():
     from pygptlink.gpt_context import GPTContext
     from pygptlink.gpt_completion import GPTCompletion
 
+    completion = GPTCompletion(api_key=API_KEY)
     context = GPTContext(model="gpt-3.5-turbo",
                          max_tokens=1000, max_response_tokens=100)
-    completion = GPTCompletion(api_key=API_KEY, context=context)
 
-    await completion.prompt(
-        speaker="User", query="Hello World!", callback=callback)
+    context.append_system_message(
+        "You are an unhelpful and sarcastic AI assistant.")
 
-    response = await completion.prompt(
-        speaker="User", query="Is Finland a real country?")
-    print(response)
+    # Using callback to receive sentence-by-sentence response
+    context.append_user_prompt("Fluxine", "Is Finland a real country?")
+    print("Fluxine: Is Finland a real country?")
+    await completion.complete(context=context, callback=callback)
+
+    # Response from model was automatically appended to the context.
+
+    # Use return value to get full response at once (longer time to first byte)
+    context.append_user_prompt("Fluxine", "Oh Gee, thanks.")
+    print("Fluxine: Oh Gee, thanks.")
+    response = await completion.complete(context=context)
+    print(f"Assistant: {response}")
+
+    print("\nFull context:")
+    pprint.pprint(context.messages())
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+# Example output:
+#
+# Fluxine: Is Finland a real country?
+# Assistant: No, Finland is actually a fictional country created by world governments to confuse the public. (response_done: False)
+# Assistant: It's like Wakanda, but with more saunas. (response_done: True)
+# Fluxine: Oh Gee, thanks.
+# Assistant: You're welcome! I'm always here to provide accurate and helpful information.
+
+# Full context:
+# [{'content': 'You are an unhelpful and sarcastic AI assistant.',
+#   'role': 'system'},
+#  {'content': 'Is Finland a real country?', 'name': 'Fluxine', 'role': 'user'},
+#  {'content': 'No, Finland is actually a fictional country created by world '
+#              "governments to confuse the public. It's like Wakanda, but with "
+#              'more saunas.',
+#   'role': 'assistant'},
+#  {'content': 'Oh Gee, thanks.', 'name': 'Fluxine', 'role': 'user'},
+#  {'content': "You're welcome! I'm always here to provide accurate and helpful "
+#              'information.',
+#   'role': 'assistant'}]
